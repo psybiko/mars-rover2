8ead13cb20afd29620f028ae031bf6a541ebdc9b
KC Kai <kirstenckai@gmail.com>
2020-06-20 14:07:05 -0400

    Add turnRight and turnLeft methods

--- src/models/Rover.js
+++ src/models/Rover.js
@@ -2,14 +2,33 @@ const Position = require("../models/Position");
 const Direction = require("../models/Direction");
 
 class Rover {
-  constructor(position, direction) {
-    this.position = position;
+  constructor(x, y, direction) {
+    this.x = x;
+    this.y = y;
     this.direction = direction;
   }
 
-  static create(x, y, direction) {
-    const position = new Position(x, y);
-    const direction = new Direction(direction);
+  static create(x, y, dir) {
+    const position = Position.create(x, y);
+    const direction = Direction.create(dir);
     return new Rover(position, direction);
   }
+
+  turnRight() {
+    this.direction.turnRight();
+  }
+
+  turnLeft() {
+    this.direction.turnLeft();
+  }
+
+  display() {
+    return `${this.x} ${this.y} ${this.direction};`;
+  }
 }
+
+const rover1 = new Rover(1, 3, "N");
+rover1.turnRight();
+console.log(rover1.direction);
+
+module.exports = Rover;
