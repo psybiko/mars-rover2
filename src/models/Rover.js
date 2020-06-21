24f41d9680680b9fbc9f141fb2b939311cf2a52c
KC Kai <kirstenckai@gmail.com>
2020-06-20 22:55:51 -0400

    Add instruction method

--- src/models/Rover.js
+++ src/models/Rover.js
@@ -22,13 +22,31 @@ class Rover {
     this.direction.turnLeft();
   }
 
+  moveForward() {
+    if (this.direction.value === "N") {
+      this.position.y += 1;
+    } else if (this.direction.value === "W") {
+      this.position.x -= 1;
+    } else if (this.direction.value === "S") {
+      this.position.y -= 1;
+    } else if (this.direction.value === "E") {
+      this.position.x += 1;
+    }
+  }
+
+  instruction(i) {
+    if (i === "L") {
+      this.turnLeft();
+    } else if (i === "R") {
+      this.turnRight();
+    } else if (i === "M") {
+      this.moveForward();
+    }
+  }
+
   display() {
     return `${this.x} ${this.y} ${this.direction};`;
   }
 }
-
-const rover1 = new Rover(1, 3, "N");
-rover1.turnRight();
-console.log(rover1.direction);
 
 module.exports = Rover;
