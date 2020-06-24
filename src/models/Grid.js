f8d0241031a1909618b3459b10eb568bb35ead65
KC Kai <kirstenckai@gmail.com>
2020-06-24 19:17:15 -0400

    Add static create method to Grid class

--- src/models/Grid.js
+++ src/models/Grid.js
@@ -4,6 +4,13 @@ class Grid {
     this.y = y;
   }
 
+  static create(x, y) {
+    if (x < 0 || y < 0) {
+      throw new Error("grid size cannot be smaller than 0, 0");
+    }
+    return new Grid(x, y);
+  }
+
   display() {
     return `${this.x}, ${this.y}`;
   }
