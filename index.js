aff9b03b1d5f043d7910f266e83c20cee1d32e7e
KC Kai <kirstenckai@gmail.com>
2020-06-24 19:24:03 -0400

    Remove Direction and Position import

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Grid = require("./src/models/Grid");
-const Position = require("./src/models/Position");
 const Rover = require("./src/models/Rover");
-const Direction = require("./src/models/Direction");
 
 /*
 Input: 
 
-5 5
+5 5 
 1 2 N 
 LMLMLMLMM 
 33E 
@@ -19,10 +17,17 @@ Expected Output:
 
 */
 
-const rover1 = Rover.create(1, 2, "N");
+// connect grid to the rover
+// if the position of the rover is bigger than the x and y values of the grid, then throw an error
+// pass instructions to the rover
+
+const grid = Grid.create(5, 5);
+console.log(grid.display());
+const rover1 = Rover.create(4, 4, "N");
 rover1.instructions("LMLMLMLMM");
 rover1.turnLeft;
 console.log(rover1.display());
-const rover2 = Rover.create(3, 3, "E");
-rover2.instructions("MMRMMRMRRM");
-console.log(rover2.display());
+
+// const rover2 = Rover.create(3, 3, "E");
+// rover2.instructions("MMRMMRMRRM");
+// console.log(rover2.display());
