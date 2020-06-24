6b550a1db317bd57b430890e8f3bbf9010c15014
KC Kai <kirstenckai@gmail.com>
2020-06-24 19:16:29 -0400

    Add create method to position class

--- src/models/Position.js
+++ src/models/Position.js
@@ -4,8 +4,16 @@ class Position {
     this.y = y;
   }
 
+  static create(x, y) {
+    if (x < 0 || y < 0) {
+      throw new Error("position cannot be smaller than grid size");
+    } else {
+      return `${x}, ${y}`;
+    }
+  }
+
   display() {
-    return `${this.x} ${this.y}`;
+    return `${this.x}, ${this.y}`;
   }
 }
 
