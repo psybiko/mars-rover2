6835ac0d783c56c52e393224d1a73af87c10f03b
KC Kai <kirstenckai@gmail.com>
2020-06-24 23:56:03 -0400

    Remove comments

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const Grid = require("./src/models/Grid");
 const Rover = require("./src/models/Rover");
 
+const grid = Grid.create(5, 5);
+console.log("GRID: ", grid);
+
+const rover1 = Rover.create(1, 2, "N");
+rover1.instructions("LMLMLMLMM");
+console.log("ROVER 1: ", rover1.display());
+
+// const rover2 = Rover.create(3, 3, "E");
+// rover2.instructions("MMRMMRMRRM");
+// console.log(rover2.display());
+
 /*
 Input: 
 
@@ -16,18 +27,3 @@ Expected Output:
 5 1 E
 
 */
-
-// connect grid to the rover
-// if the position of the rover is bigger than the x and y values of the grid, then throw an error
-// pass instructions to the rover
-
-const grid = Grid.create(5, 5);
-console.log(grid.display());
-const rover1 = Rover.create(4, 4, "N");
-rover1.instructions("LMLMLMLMM");
-rover1.turnLeft;
-console.log(rover1.display());
-
-// const rover2 = Rover.create(3, 3, "E");
-// rover2.instructions("MMRMMRMRRM");
-// console.log(rover2.display());
