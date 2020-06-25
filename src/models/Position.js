3960e834f1fd9967532ead25a857210e148cbfdc
KC Kai <kirstenckai@gmail.com>
2020-06-24 23:56:16 -0400

    Remove static create method

--- src/models/Position.js
+++ src/models/Position.js
@@ -4,14 +4,6 @@ class Position {
     this.y = y;
   }
 
-  static create(x, y) {
-    if (x < 0 || y < 0) {
-      throw new Error("position cannot be smaller than grid size");
-    } else {
-      return `${x}, ${y}`;
-    }
-  }
-
   display() {
     return `${this.x}, ${this.y}`;
   }
